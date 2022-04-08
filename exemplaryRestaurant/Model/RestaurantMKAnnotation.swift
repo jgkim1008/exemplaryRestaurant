b9e1b3abf4c896f70f852abbe185a368bf451073
jgkim1008 <rlawnsrjs100@naver.com>
2022-04-08 20:21:25 +0900

    feat: RestaurantMKAnnotation 타입 구현
    * MKMapView에 보여줄 모델타입 구현
    * 위도,경도 프로퍼티 변경

---
+++ exemplaryRestaurant/Model/RestaurantMKAnnotation.swift
@@ -0,0 +1,29 @@
+//
+//  RestaurantMKAnnotation.swift
+//  exemplaryRestaurant
+//
+//  Created by 김준건 on 2022/04/06.
+//
+
+import MapKit
+import Foundation
+
+final class RestaurantMKAnnotation: NSObject, MKAnnotation {
+    let title: String?
+    let subtitle: String?
+    let locationName: String?
+    let type: String?
+    var coordinate: CLLocationCoordinate2D
+    
+    
+    init(title: String?, subtitle: String?, locationName: String?, type: String?, coordinate: CLLocationCoordinate2D) {
+        self.title = title
+        self.subtitle = subtitle
+        self.locationName = locationName
+        self.type = type
+        self.coordinate = coordinate
+    }
+    
+ 
+    
+}
